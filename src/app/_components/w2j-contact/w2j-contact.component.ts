30e984ce48f411cebadc7e465ee7754f686cc5e1
gustah90 <gustavohdsp@yahoo.com>
2022-02-13 18:56:30 -0300

    Resposivity: Ok

---
+++ src/app/_components/w2j-contact/w2j-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-w2j-contact',
+  templateUrl: './w2j-contact.component.html',
+  styleUrls: ['./w2j-contact.component.scss']
+})
+export class W2jContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
