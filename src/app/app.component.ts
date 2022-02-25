7136971e71b93095002448b50626645553d52f01
gustah90 <67840882+gustah90@users.noreply.github.com>
2022-02-25 08:37:33 -0300

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from '../app/shared/domain/enum/language.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'w2j';
+
+  constructor(private translate: TranslateService) {
+    const browserLang = this.translate.getBrowserCultureLang();
+
+    switch (browserLang) {
+      case Language.PORTUGUESE:
+        this.translate.setDefaultLang(Language.PORTUGUESE);
+        break;
+      case Language.SPANISH:
+        this.translate.setDefaultLang(Language.SPANISH);
+        break;
+      default:
+        this.translate.setDefaultLang(Language.ENGLISH);
+        break;
+    }
+  }
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
 
   isOpenMenu = false;
 
---
+++ src/app/core/_contact/w2j-contact.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'w2j-contact',
+  templateUrl: './w2j-contact.component.html',
+  styleUrls: ['./w2j-contact.component.scss']
+})
+export class W2jContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
